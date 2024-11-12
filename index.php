2c2c1835184a54c864f4ed0cb74d611bea5d395c
agskanchana <sameerakanchana@live.com>
2024-11-12 11:30:51 +0530

    removed party town js configs

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: Ekwa Settings
 Plugin URI: www.ekwa.com
 Description: Loading theird party scripts from service worker, add Progressive web app
 Author URI: www.sameera.com
-Version: 1.3.7
+Version: 1.3.8
 
 */
 
@@ -90,7 +90,6 @@ if( !function_exists('carbon_fields_boot_plugin')){
         Field::make( 'text', 'fb_no_script', __( 'No script Image Source' ) ),
         Field::make( 'select', 'fb_method', __( 'Method' ) )
         ->set_options( array(
-            'Service Worker' => 'Service Worker',
             'Mouse moment' => 'Mouse moment',
             'General' => 'General',
         ) ),
@@ -388,27 +387,7 @@ console.log('SW registration failed with error:', e);
 }
 add_action( 'wp_head',  'show_pwa_on_header', 1 );
 
-function partytown_configuration() {
-    if(carbon_get_theme_option('method') == 'Service Worker'){
 
-	$config = array(
-		'lib' => str_replace( site_url(), '', plugin_dir_url( __FILE__ ) ) . 'js/partytown/',
-	);
-
-
-	$config = apply_filters( 'partytown_configuration', $config );
-
-	?>
-	<script>
-		window.partytown = <?php echo wp_json_encode( $config ); ?>;
-        /* party town js*/
-        !function(t,e,n,i,r,o,a,d,s,c,l,p){function u(){p||(p=1,"/"==(a=(o.lib||"/~partytown/")+(o.debug?"debug/":""))[0]&&(s=e.querySelectorAll('script[type="text/partytown"]'),i!=t?i.dispatchEvent(new CustomEvent("pt1",{detail:t})):(d=setTimeout(f,1e4),e.addEventListener("pt0",w),r?h(1):n.serviceWorker?n.serviceWorker.register(a+(o.swPath||"partytown-sw.js"),{scope:a}).then((function(t){t.active?h():t.installing&&t.installing.addEventListener("statechange",(function(t){"activated"==t.target.state&&h()}))}),console.error):f())))}function h(t){c=e.createElement(t?"script":"iframe"),t||(c.setAttribute("style","display:block;width:0;height:0;border:0;visibility:hidden"),c.setAttribute("aria-hidden",!0)),c.src=a+"partytown-"+(t?"atomics.js?v=0.8.0":"sandbox-sw.html?"+Date.now()),e.querySelector(o.sandboxParent||"body").appendChild(c)}function f(n,r){for(w(),i==t&&(o.forward||[]).map((function(e){delete t[e.split(".")[0]]})),n=0;n<s.length;n++)(r=e.createElement("script")).innerHTML=s[n].innerHTML,e.head.appendChild(r);c&&c.parentNode.removeChild(c)}function w(){clearTimeout(d)}o=t.partytown||{},i==t&&(o.forward||[]).map((function(e){l=t,e.split(".").map((function(e,n,i){l=l[i[n]]=n+1<i.length?"push"==i[n+1]?[]:l[i[n]]||{}:function(){(t._ptf=t._ptf||[]).push(i,arguments)}}))})),"complete"==e.readyState?u():(t.addEventListener("DOMContentLoaded",u),t.addEventListener("load",u))}(window,document,navigator,top,window.crossOriginIsolated);
-	</script>
-	<?php
-    }
-}
-
-add_action( 'wp_head',  'partytown_configuration', 1 );
 
 function print_tracking_codes_in_header(){
     if(carbon_get_theme_option('fb_no_script')){
@@ -426,12 +405,12 @@ function print_tracking_codes(){
     if(carbon_get_theme_option('ga_show_custom')){
         if(carbon_get_theme_option('ga_script_link')){
             ?>
-                <script  type="text/partytown" src="<?php echo carbon_get_theme_option('ga_script_link');?>"></script>
+                <script  type="text/javascript"  src="<?php echo carbon_get_theme_option('ga_script_link');?>"></script>
             <?php
         }
         if(carbon_get_theme_option('ga_custom_script')){
             ?>
-                <script type="text/partytown">
+                <script  type="text/javascript">
                     <?php echo  carbon_get_theme_option('ga_custom_script');?>
                 </script>
             <?php
@@ -442,16 +421,15 @@ function print_tracking_codes(){
              if(carbon_get_theme_option('method') == 'Service Worker'){
 
             ?>
-                <script  type="text/partytown" async src="https://www.googletagmanager.com/gtag/js?id=<?php echo carbon_get_theme_option('measurement_id');?>"></script>
-                <script  type="text/partytown">
+                 <script   async src="https://www.googletagmanager.com/gtag/js?id=<?php echo carbon_get_theme_option('measurement_id');?>"></script>
+                <script>
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
                 gtag('config', '<?php echo carbon_get_theme_option('measurement_id');?>');
                 </script>
-
-            <?php
+                <?php
             }
             if(carbon_get_theme_option('method') == 'Mouse moment'){
                 ?>
@@ -478,8 +456,8 @@ function print_tracking_codes(){
     if(carbon_get_theme_option('fb_pixel_code')){
         if(carbon_get_theme_option('fb_method') == 'Service Worker'){
         ?>
-        <script  type="text/partytown">
-            <?php echo carbon_get_theme_option('fb_pixel_code');?>
+       <script>
+        <?php echo carbon_get_theme_option('fb_pixel_code');?>
         </script>
 
         <?php
