63f9495fc43448a2e07054ed458a0c691e3d789c
agskanchana <sameerakanchana@live.com>
2025-05-07 21:23:15 +0530

    updated version with align

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: Ekwa Settings
 Plugin URI: www.ekwa.com
 Description: Loading theird party scripts from service worker, add Progressive web app
 Author URI: www.sameera.com
-Version: 1.9.3
+Version: 1.9.4
 
 */
 
